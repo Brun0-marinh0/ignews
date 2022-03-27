edcfe3dc084db29217bc3275034290d4711af489
Bruno_Marinho <bcaldasmarinhojklb@gmail.com>
2022-03-27 17:53:51 -0300

    tela home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
     <Head>
-        <title>Inicio | ig.news</title>
-      </Head>
+        <title>Home | ig.news</title>
+    </Head>
+    <main className={styles.contantContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, wellcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get acess to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+    </main>
     </>
   )
 }
